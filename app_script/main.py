d36c31bdc66a87b6aec3d4ea88b4d0637d6a6c63
chaiwit.p <chaiwit.p@groupmaker.co.th>
2022-12-09 06:58:11 +0700

    update reconnect

--- app_script/main.py
+++ app_script/main.py
@@ -13,16 +13,17 @@ def thread_stream(id_stream, rtsp_url, live_url, key):
         print('Live URL: ' + live_url)
         print('Key: ' + key)
         print("StreamingTask is running")
-        ffcmd = f"ffmpeg -an -rtsp_transport tcp -stimeout 30000000 -i {rtsp_url} -tune zerolatency -vcodec libx264 -pix_fmt + -c:v copy -f flv rtmp://127.0.0.1/live/{live_url}?API_KEY={key}"
+        ffcmd = f"ffmpeg -an -rtsp_transport tcp -stimeout -1 -i {rtsp_url} -reconnect 1 -reconnect_at_eof 1 -reconnect_delay_max 4294 -reconnect_streamed 1 -tune zerolatency -vcodec libx264 -pix_fmt + -c:v copy -f flv rtmp://127.0.0.1/live/{live_url}?API_KEY={key}"
         cmd = ffcmd.split()
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
-        while True:
+        for line in process.stdout:
+            print(line, end='')
             if id_stream not in stream_is_runing:
                 print('Stop Stream: ' + id_stream)
                 threading_streams.pop(id_stream, None)
                 subprocess.Popen.kill(process)
                 break
-            sleep(0.5)
+        sleep(0.25)
     except:
         print('error restreaming')
             
@@ -41,8 +42,8 @@ def main():
                 stream_is_runing.remove(x[0])
             except:
                 pass
-            sleep(0.25)
-        sleep(0.25)
+            sleep(0.2)
+        sleep(0.2)
         db_cursor.close()
         
         db_conn = DatabaseConn().DB_CONN
@@ -52,8 +53,8 @@ def main():
         for x in result_streaming:
             stream_state[x[0]] = True
             stream_store[x[0]] = x
-            sleep(0.25)
-        sleep(0.25)
+            sleep(0.2)
+        sleep(0.2)
         db_cursor.close()
         
         # print(stream_store['SID-000001'])
@@ -65,8 +66,8 @@ def main():
                     threading_streams[key] = threading.Thread(target=thread_stream, args=(key, stream_store[key][1], stream_store[key][2], stream_store[key][3]))
                     threading_streams[key].daemon = True
                     threading_streams[key].start()
-            sleep(0.25)
-        sleep(0.25)
+            sleep(0.2)
+        sleep(0.2)
 if __name__ == '__main__':
     main()
     
